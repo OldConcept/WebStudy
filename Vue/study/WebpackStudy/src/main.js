3127eb54f362f31a8ab5392dc2cf3d79cf71d25c
Mr.Ye <oldconcept@outlook.com>
2019-11-09 18:12:21 +0800

    高级JS语法的打包

--- Vue/study/WebpackStudy/src/main.js
+++ Vue/study/WebpackStudy/src/main.js
@@ -24,6 +24,12 @@ $(function(){
     })
 })
 
+class Person{
+    static info ='aaa'
+}
+
+console.log(Person)
+
 // 通过【webpack ./src/main.js -o ./dist/bundle.js --mode production】这一条指令，能够发现：
 // 1.webpack能够处理JS文件之间的互相依赖关系
 // 2.webpack能够处理JS的兼容问题，把高级的、浏览器不识别的语法转化为低级的、浏览器能识别的语法
